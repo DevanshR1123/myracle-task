684a6f624ac65e279b5bd37839c49020501b8a9f
DevanshR1123 <65907921+DevanshR1123@users.noreply.github.com>
2024-09-07 17:08:51 +0530

    Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <>
-      <h1 className="text-center text-4xl">Hello</h1>
+      <header className="p-4">
+        <h1 className="text-center text-6xl font-bold">TestMe</h1>
+      </header>
+      <main>
+        <form action=""></form>
+      </main>
+      <footer className="bg-secondary p-2 text-center text-secondary-foreground">
+        For Myracle.io Case Study
+      </footer>
     </>
   );
 }
